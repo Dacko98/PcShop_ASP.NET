fc0747008d93a64ceeb1632fe7e44799279351ab
Martin Kmenta <martin.kmenta.dev@gmail.com>
2020-12-13 19:15:39 +0100

    added categories pages

---
+++ PcShop.Web/Pages/Categories/CategoryDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using PcShop.BL.Api.Models.Category;
+using PcShop.BL.Api.Models.Product;
+using PcShop.WEB.BL.Facades;
+
+namespace PcShop.Web.Pages.Categories
+{
+    public partial class CategoryDetail : ComponentBase
+    {
+        [Inject] private CategoriesFacade CategoryFacade { get; set; }
+        [Inject] private ProductsFacade ProductFacade { get; set; }
+
+        [Parameter] public Guid Id { get; set; }
+
+        public CategoryDetailModel Category { get; set; }
+        private ICollection<ProductListModel> Products { get; set; } = new List<ProductListModel>();
+        
+        protected override async Task OnInitializedAsync()
+        {
+            Category = await CategoryFacade.GetCategoryAsync(Id);
+            Products = await ProductFacade.GetProductsAsync();
+            await base.OnInitializedAsync();
+        }
+    }
+}
---
+++ PcShop.Web/Pages/Categories/CategoryEdit.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using PcShop.BL.Api.Models.Category;
+using System.Threading.Tasks;
+using PcShop.WEB.BL.Facades;
+using System;
+using System.Collections.Generic;
+using PcShop.BL.Api.Models.Product;
+
+namespace PcShop.Web.Pages.Categories
+{
+    public partial class CategoryEdit : ComponentBase
+    {
+        [Inject] private CategoriesFacade CategoryFacade { get; set; }
+        [Inject] private ProductsFacade ProductFacade { get; set; }
+
+        [Parameter]
+        public Guid Id { get; set; }
+
+        public CategoryDetailModel Category { get; set; }
+        private ICollection<ProductListModel> Products { get; set; } = new List<ProductListModel>();
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            if(Id == Guid.Empty)
+                Category = new CategoryDetailModel();
+            else
+                Category = await CategoryFacade.GetCategoryAsync(Id);
+            Products = await ProductFacade.GetProductsAsync();
+
+            await base.OnInitializedAsync();
+        }
+
+        protected async Task SaveData()
+        {
+            // todo
+        }
+    }
+}
