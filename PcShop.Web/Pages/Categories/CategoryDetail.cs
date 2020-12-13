e8e3981af7685c51a86ee3de9710eb613a25eb77
Martin Kmenta <martin.kmenta.dev@gmail.com>
2020-12-13 21:01:07 +0100

    additing category working

--- PcShop.Web/Pages/Categories/CategoryDetail.cs
+++ PcShop.Web/Pages/Categories/CategoryDetail.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using PcShop.BL.Api.Models.Category;
+using PcShop.BL.Api.Models.Manufacturer;
 using PcShop.BL.Api.Models.Product;
 using PcShop.WEB.BL.Facades;
 
@@ -12,17 +14,116 @@ namespace PcShop.Web.Pages.Categories
     {
         [Inject] private CategoriesFacade CategoryFacade { get; set; }
         [Inject] private ProductsFacade ProductFacade { get; set; }
+        [Inject] private ManufacturersFacade ManufacturerFacade { get; set; }
 
         [Parameter] public Guid Id { get; set; }
 
         public CategoryDetailModel Category { get; set; }
         private ICollection<ProductListModel> Products { get; set; } = new List<ProductListModel>();
-        
+
+        private ICollection<ProductListModel> AllProducts { get; set; } = new List<ProductListModel>();
+        private ICollection<ManufacturerListModel> Manufacturers { get; set; } = new List<ManufacturerListModel>();
+
+        public System.Collections.Generic.List<string> SelectedValues { get; set; } = new System.Collections.Generic.List<string>();
+
+        public string category = "All";
+        public int priceStart { get; set; } = 0;
+        public int priceEnd { get; set; } = Int32.MaxValue;
+
+        public int weightStart { get; set; } = 0;
+        public int weightEnd { get; set; } = Int32.MaxValue;
+
+        public bool inStock { get; set; } = false;
+
         protected override async Task OnInitializedAsync()
         {
-            Category = await CategoryFacade.GetCategoryAsync(Id);
-            Products = await ProductFacade.GetProductsAsync();
+            await LoadData();
+
             await base.OnInitializedAsync();
+        }
+
+        private async Task LoadData()
+        {
+            Category = await CategoryFacade.GetCategoryAsync(Id);
+            Products = (await ProductFacade.GetProductsAsync())
+                .ToList().FindAll(p=>p.CategoryName == Category.Name);
+
+            AllProducts = Products;
+            Manufacturers = await ManufacturerFacade.GetManufacturersAsync();
+        }
+
+        public void PriceStart(ChangeEventArgs e)
+        {
+            string val = e.Value.ToString();
+            priceStart = val.Equals("") ? 0 : Int32.Parse(val);
+
+            ApplyFilters();
+        }
+        public void PriceEnd(ChangeEventArgs e)
+        {
+            string val = e.Value.ToString();
+            priceEnd = val.Equals("") ? Int32.MaxValue : Int32.Parse(val);
+
+            ApplyFilters();
+        }
+
+        public void WeightStart(ChangeEventArgs e)
+        {
+            string val = e.Value.ToString();
+
+            weightStart = val.Equals("") ? 0 : Int32.Parse(val);
+
+
+            ApplyFilters();
+        }
+        public void WeightEnd(ChangeEventArgs e)
+        {
+            string val = e.Value.ToString();
+            weightEnd = val.Equals("") ? Int32.MaxValue : Int32.Parse(val);
+
+            ApplyFilters();
+        }
+
+
+        public void CheckBoxChecked(string aSelectedId, object aChecked)
+        {
+            {
+                if ((bool)aChecked)
+                {
+                    if (!SelectedValues.Contains(aSelectedId))
+                    {
+                        SelectedValues.Add(aSelectedId);
+                    }
+                }
+                else
+                {
+                    if (SelectedValues.Contains(aSelectedId))
+                    {
+                        SelectedValues.Remove(aSelectedId);
+                    }
+                }
+            }
+            ApplyFilters();
+        }
+
+        public void Stock(object aChecked)
+        {
+
+            inStock = ((bool)aChecked);
+            ApplyFilters();
+        }
+
+        public void ApplyFilters()
+        {
+
+            Products = category.Equals("All") ? AllProducts : AllProducts.Where(f => f.CategoryName.Equals(category)).ToList();
+            Products = !SelectedValues.Any() ? Products : Products.Where(f => SelectedValues.Contains(f.ManufacturerName)).ToList();
+            Products = Products.Where(f => f.Price <= priceEnd && f.Price >= priceStart).ToList();
+            Products = Products.Where(f => f.Weight <= weightEnd && f.Weight >= weightStart).ToList();
+            if (inStock)
+            {
+                Products = Products.Where(f => f.CountInStock > 0).ToList();
+            }
         }
     }
 }
--- PcShop.Web/Pages/Categories/CategoryEdit.cs
+++ PcShop.Web/Pages/Categories/CategoryEdit.cs
@@ -3,37 +3,38 @@ using PcShop.BL.Api.Models.Category;
 using System.Threading.Tasks;
 using PcShop.WEB.BL.Facades;
 using System;
-using System.Collections.Generic;
-using PcShop.BL.Api.Models.Product;
+using System.Diagnostics;
 
 namespace PcShop.Web.Pages.Categories
 {
     public partial class CategoryEdit : ComponentBase
     {
         [Inject] private CategoriesFacade CategoryFacade { get; set; }
-        [Inject] private ProductsFacade ProductFacade { get; set; }
 
-        [Parameter]
-        public Guid Id { get; set; }
+        [Parameter] public Guid Id { get; set; }
 
         public CategoryDetailModel Category { get; set; }
-        private ICollection<ProductListModel> Products { get; set; } = new List<ProductListModel>();
 
+        private bool NewCategory { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            if(Id == Guid.Empty)
-                Category = new CategoryDetailModel();
-            else
-                Category = await CategoryFacade.GetCategoryAsync(Id);
-            Products = await ProductFacade.GetProductsAsync();
+            NewCategory = Id == Guid.Empty;
+            Category = NewCategory ? new CategoryDetailModel() : await CategoryFacade.GetCategoryAsync(Id);
 
             await base.OnInitializedAsync();
         }
 
         protected async Task SaveData()
         {
-            // todo
+            Guid response;
+            if (NewCategory)
+                response = await CategoryFacade.CreateAsync(new CategoryNewModel {Name = Category.Name});
+            else
+                response = await CategoryFacade.UpdateAsync(new CategoryUpdateModel {Name = Category.Name, Id = Id});
+
+            Debug.WriteLine("Data should be saved    " + Id + "   " + Category.Name);
+            Debug.WriteLine("Saving returns:   " + response);
         }
     }
 }
