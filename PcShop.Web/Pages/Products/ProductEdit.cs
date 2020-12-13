a1d679c87cef5ec53cc4ef47293b45ca8d15c8ed
vlachvojta <vojtik.vlach@seznam.cz>
2020-12-13 19:00:03 +0100

    Product edit form almost DONE.
    Not saving all data yet. TODO: save Evaluations, Manufacturer and category

--- PcShop.Web/Pages/Products/ProductEdit.cs
+++ PcShop.Web/Pages/Products/ProductEdit.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using PcShop.WEB.BL.Facades;
 using System.Net.Http.Json;
 using System.Diagnostics;
+using Newtonsoft.Json;
 using System.Net.Http;
 using System.Linq;
 using System;
@@ -31,47 +32,52 @@ namespace PcShop.Web.Pages.Products
         [Inject]
         private EvaluationsFacade EvaluationsFacade { get; set; }
 
-        private bool NewCategory = false;
-        private bool NewManufacturer = false;
+        private bool createNewCategory = false;
+        private bool createNewManufacturer = false;
         private bool createNewEvaluation = false;
         private EvaluationNewModel NewEvaluation = new EvaluationNewModel();
+        private ManufacturerNewModel newManufacturer = new ManufacturerNewModel();
 
         //private ICollection<ProductListModel> Products { get; set; } = new List<ProductListModel>();
 
         //private ICollection<ProductListModel> AllProducts { get; set; } = new List<ProductListModel>();
         private ICollection<ManufacturerListModel> Manufacturers { get; set; } = new List<ManufacturerListModel>();
-        private ICollection<CategoryListModel> Category { get; set; } = new List<CategoryListModel>();
+        private ICollection<CategoryListModel> Categories { get; set; } = new List<CategoryListModel>();
         private ICollection<EvaluationListModel> Evaluations { get; set; } = new List<EvaluationListModel>();
 
         protected override async Task OnInitializedAsync()
         {
-            if(Id == Guid.Empty)
+            if (Id == Guid.Empty)
                 product = new ProductDetailModel();
             else
                 product = await ProductFacade.GetProductAsync(Id);
 
             Manufacturers = await ManufacturerFacade.GetManufacturersAsync();
-            Category = await CategoryFacade.GetCategorysAsync();
+            Categories = await CategoryFacade.GetCategorysAsync();
             Evaluations = await EvaluationsFacade.GetEvaluationsAsync();
-            
+
             await base.OnInitializedAsync();
         }
 
         protected async Task SaveData()
         {
             Debug.WriteLine("Data should be saved");
+
             // UpdateAsync(product) or something
             // actualize all evaluations 
             // push the new evaluation if there is one
+            // save new manufacturer name to product
+            // push new category and manufacturer
+            await ProductFacade.UpdateAsync(await UpdateProduct());
         }
 
         public void categorySelect(ChangeEventArgs e)
         {
             if (e.Value.ToString() == "new category")
-                NewCategory = true;
+                createNewCategory = true;
             else
             {
-                NewCategory = false;
+                createNewCategory = false;
                 product.CategoryName = e.Value.ToString();
             }
         }
@@ -79,26 +85,108 @@ namespace PcShop.Web.Pages.Products
         public void manufacturerSelect(ChangeEventArgs e)
         {
             if (e.Value.ToString() == "new manufacturer")
-                NewManufacturer = true;
+            {
+                createNewManufacturer = true;
+                newManufacturer = new ManufacturerNewModel();
+            }
             else
             {
-                NewManufacturer = false;
+                createNewManufacturer = false;
                 product.ManufacturerName = e.Value.ToString();
             }
         }
 
-        protected void AddEvaluation()
+        public void AddEvaluation()
         {
             // TODO - tadyto nefunguje, ten btn odkazuje na SaveData, idk why...
-            Debug.WriteLine("it is working");
+
+            Debug.WriteLine("Adding evaluation is working");
             if (createNewEvaluation)
             {
+                Debug.WriteLine("Evaluation percent is: " + NewEvaluation.ProductId + ".");
+
                 // There is one already, should be pushed or something.
             }
             else
             {
                 createNewEvaluation = true;
             }
+        }
+
+        public async Task<ProductUpdateModel> UpdateProduct()
+        {
+            ProductUpdateModel productUpdateModel = new ProductUpdateModel()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Photo = product.Photo,
+                Description = product.Description,
+                Price = product.Price,
+                Weight = product.Weight,
+                CountInStock = product.CountInStock,
+                RAM = product.RAM,
+                CPU = product.CPU,
+                GPU = product.GPU,
+                HDD = product.HDD,
+                ManufacturerId = await DecideNewManufacturer(),
+                CategoryId = await DecideNewCategory(),
+                Evaluations = 
+                new List<EvaluationUpdateModel>()
+                // Evaluations = product.Evaluations
+                //Evaluations = ListOfEvalutaion()
+            };
+            return productUpdateModel;
+        }
+
+        public async Task<Guid> DecideNewManufacturer()
+        {
+            foreach (var manufacturer in Manufacturers)
+            {
+                if (manufacturer.Name == product.ManufacturerName)
+                    return manufacturer.Id;
+            }
+
+            // create new Manufacturer 
+
+            return Guid.Empty;
+        }
+
+        public async Task<Guid> DecideNewCategory()
+        {
+            foreach (var category in Categories)
+            {
+                if (category.Name == product.CategoryName)
+                    return category.Id;
+            }
+
+            CategoryNewModel newCategoryModel = new CategoryNewModel() { Name = product.CategoryName };
+            var response =  await CategoryFacade.CreateAsync(newCategoryModel);
+
+            Debug.WriteLine("NewCategory response: " + response.ToString());
+            Debug.WriteLine("NewCategory name: " + product.CategoryName);
+            return response;
+        }
+
+        public Guid FindManufacturerByName(string ManufacturerName)
+        {
+            foreach(var manufacturer in Manufacturers)
+            {
+                if (manufacturer.Name == ManufacturerName)
+                    return manufacturer.Id;
+            }
+
+            return Guid.Empty;  // shouldn't come to this
+        }
+
+        public Guid FindCategoryByName(string CategoryName)
+        {
+            foreach (var category in Categories)
+            {
+                if (category.Name == CategoryName)
+                    return category.Id;
+            }
+
+            return Guid.Empty;  // shouldn't come to this
         }
     }
 }
